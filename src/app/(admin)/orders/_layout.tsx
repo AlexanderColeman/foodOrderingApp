0456af079042612a7f23a451e76e022d0ea226e8
Alexander Coleman <AlexanderJColeman01@gmail.com>
2025-02-16 17:00:36 -0600

    Add TopTab navigator

--- src/app/(admin)/orders/_layout.tsx
+++ src/app/(admin)/orders/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function MenuStack() {
   return (
     <Stack screenOptions={{}}>
-      <Stack.Screen name="index" options={{ title: "Orders" }} />
+      <Stack.Screen name="list" options={{ headerShown: false }} />
     </Stack>
   );
 }
