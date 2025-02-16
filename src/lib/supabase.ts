c4d0bfa149036fe23f64fc4d759b110429424122
Alexander Coleman <AlexanderJColeman01@gmail.com>
2025-02-16 17:51:12 -0600

    Config supabase add login and signup functions

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import "react-native-url-polyfill/auto";
+import * as SecureStore from "expo-secure-store";
+import { createClient } from "@supabase/supabase-js";
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = "https://fawwyubbexbcwxwumeki.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZhd3d5dWJiZXhiY3d4d3VtZWtpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk3NDcyODIsImV4cCI6MjA1NTMyMzI4Mn0.ZTEX-5_itiee4q6SyjtjSz1OelNlJCf-UiImLYX-QGY";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
